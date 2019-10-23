f808c1367fed3eaf434504489b15fe461b5a4e99
Jan P <janman74@gmail.com>
2019-10-23 15:14:57 -0500

    Add and Implement Cart

---
+++ ShoppingCartTests/CartTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ShoppingCart;
+
+namespace ShoppingCartTests
+{
+    [TestFixture]
+    class CartTests
+    {
+        private int _zero;
+        private Cart _cart;
+        private CartItem _cartItem;
+        private CartItem _cartItem2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _zero = 0;
+            _cart = new Cart();
+            _cartItem = new CartItem(2, "Test description", 12.00m);
+            _cartItem2 = new CartItem(4, "Diffrent description", 5.00m);
+        }
+
+        [Test]
+        public void CartCanContainZeroItems()
+        {
+            Assert.That(_cart.Items.Count, Is.EqualTo(_zero));
+        }
+
+        [Test]
+        public void CartCanAddItem()
+        {
+            _cart.AddItem(_cartItem);
+
+            Assert.That(_cart.Items, Has.Member(_cartItem));
+        }
+
+        [Test]
+        public void CartContainsNoDuplicateItems()
+        {
+            _cart.AddItem(_cartItem);
+            _cart.AddItem(_cartItem);
+
+            Assert.That(_cart.Items.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CartCanRemoveItem()
+        {
+            _cart.AddItem(_cartItem);
+            _cart.RemoveItem(_cartItem);
+
+            Assert.That(_cart.Items.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TotalEqualsSumOfItemPrices()
+        {
+            _cart.AddItem(_cartItem);
+            _cart.AddItem(_cartItem2);
+            var actualTotal = _cart.GetCartTotal();
+
+            Assert.That(actualTotal, Is.EqualTo(44.00m));
+
+        }
+    }
+}
